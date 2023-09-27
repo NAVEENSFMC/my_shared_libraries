fe21b2e2f6c3a5e48c5feb7baa54fb33f35e0b6c
Naveen <devops.navinn@gmail.com>
2023-09-27 23:52:05 +0100

    file add updated

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,10 +1,10 @@
 @Library('naveen_it_shared_lib') _
 def add(x,y){
-	echo "The sum of ${x} + ${y} is ${x+y}"
+	echo "The sum of ${x} and ${y} is ${x+y}"
 	}
 
 def mul(x,y) {
-	echo "The multiply of ${x} * ${y} is ${x*y}"
+	echo "The multiply of ${x} and ${y} is ${x*y}"
 	}
 def call(welcome){
 	echo "welcome to calculator"
