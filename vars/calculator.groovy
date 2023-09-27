e6ce9fa8cb5a08953a1a396ec7f4579cc2b65b07
Naveen <devops.navinn@gmail.com>
2023-09-27 23:30:31 +0100

    file added cal

---
+++ vars/calculator.groovy
@@ -0,0 +1,12 @@
+@Library('naveen_it_shared_lib') _
+def add(x,y){
+	echo "The sum of ${x} + ${y} is ${x+y}"
+	}
+
+def mul(x,y) {
+	echo "The multiply of ${x} * ${y} is ${x*y}"
+	}
+def call(welcome){
+	echo "welcome to calculator"
+	}
+ 
